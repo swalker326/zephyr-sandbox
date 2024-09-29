e73fa6e1ed0c44fc99d731dbf8ff20a4b1302f53
Shane <shane@swalker.dev>
2024-09-29 11:04:52 -0500

    initial commit, working tests and application

---
+++ apps/host/src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./app.module.css";
+
+export function App() {
+	return (
+		<div>
+			<h1>Welcome</h1>
+		</div>
+	);
+}
+
+export default App;
